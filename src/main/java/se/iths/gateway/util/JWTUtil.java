22cc8d849a263931ed37486b7d4208698cf9571c
Patrik Fallqvist Magnusson <patrik.fallqvist@gmail.com>
2022-05-20 16:32:30 +0200

    Add environment variable for public key to use in consul config

--- src/main/java/se/iths/gateway/util/JWTUtil.java
+++ src/main/java/se/iths/gateway/util/JWTUtil.java
@@ -3,6 +3,7 @@ package se.iths.gateway.util;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.KeyFactory;
@@ -15,8 +16,11 @@ import java.util.Base64;
 @Component
 public class JWTUtil {
 
+    @Value("${key.public}")
+    private String encodedKey;
+
     public Jws<Claims> getAllClaimsFromToken(String authToken) throws NoSuchAlgorithmException, InvalidKeySpecException {
-        String base64EncodedKey = getBase64EncodedKey();
+        String base64EncodedKey = encodedKey;
 
         byte[] byteKey = Base64.getDecoder().decode(base64EncodedKey.getBytes());
         X509EncodedKeySpec x509PublicKey = new X509EncodedKeySpec(byteKey);
@@ -28,9 +32,5 @@ public class JWTUtil {
                 .setSigningKey(publicKey)
                 .build()
                 .parseClaimsJws(authToken);
-    }
-
-    private String getBase64EncodedKey() {
-        return "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAxRDTrtKTqzK/Muw9A34JrnBYHQoMp75oPktLLzHRA21of03OTcykLM2tm0EZ9uKDbvA0PMmA16FEYO2dla35BSzxUt2wmhdOCMipi73Jkafj54U2dwW5wxvYN1WOedOAE+7/PnCa9DYTSg8NmOYoKZcZf0UUGCxcIIWRbvmaUUw8yFmcpfou2rTEtA+uKabn4Hc1j6OR035yq8UwasqxmJ9VcOxVeDenF1iV3BGxvan7zzR9I9Npt7Q2WggC7fz9rxYbnpigu1HkJOzR5GWijjjGa4xR/o2rgHZHaix8oP+rQbtJD9qmckY7DZgRaVq8yaYxFb2Q1fq0VIuJAgLz+QIDAQAB";
     }
 }
